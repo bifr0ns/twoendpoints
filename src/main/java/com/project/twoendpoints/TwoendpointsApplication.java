6b3e11dc5da748f295fcf9197e6c174a82751860
Hector Trejo <hate2411@gmail.com>
2021-08-26 18:49:18 -0500

    Initial commit

---
+++ src/main/java/com/project/twoendpoints/TwoendpointsApplication.java
@@ -0,0 +1,13 @@
+package com.project.twoendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoendpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoendpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/twoendpoints/TwoendpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.twoendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoendpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
